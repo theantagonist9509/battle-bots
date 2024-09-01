0424962d1b20e55e362be1748a75d920a003a2ae
Tejas Singh <tejas.tanmay595@gmail.com>
2024-09-01 17:20:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Battle Bots
+
+![example/scene.png](https://github.com/theantagonist9509/battle-bots/blob/main/battle-bots.png)
+
+## Problem Statement
+
+You are tasked with writing a Python script that controls a bot that can play the game of Tic Tac Toe.
+
+## Judging
+
+* Your bots will be compete against each other in a [round-robin tournament](https://en.wikipedia.org/wiki/Round-robin_tournament).
+* For each match, you will be awarded 0, 0.5, or 1 point for a loss, draw, or win respectively.
+* The final standings will be generated based on the total tournament points of the bots.
+
+## Participation
+
+* Teams can consist of up to two members. Solo participation is also allowed.
+* Each team can submit only one bot.
+* No prior experience in AI or advanced programming is required – this challenge is designed for beginners!
+
+## Submission
+
+* All submissions must be made through [this]() Google Form.
+* The submission should include:
+  1. Your **single file** Python script (named as `teamname_bot.py`)
+  2. A brief description explaining your bot's strategy (doesn't need to be very detailed!)
+* The deadline for submissions is 7th September, 11:59 PM.
+* Late submissions will not be accepted.
+* Make sure your code runs without errors and follows the input/output format as explained in the `example_bot.py` file in this repository.
---
+++ example_bot.py
@@ -0,0 +1,61 @@
+from GameState import GameState
+from Action import Action
+from threading import Thread
+
+# For the exmple bot that plays randomly; you probably don't need this line for your own bot though
+import random
+
+# This is the Player class
+# You will make changes here in order to create a bot which can play the game
+class Player(Thread):
+
+    # *args is internally used; do not touch!
+    def __init__(self, my_variables_initial_value, *args):
+
+        # Declare/initialize your variables here
+        self.my_variable = my_variables_initial_value
+
+        # Internally used code; do not touch these 3 lines!
+        Thread.__init__(self)
+        self.args = args
+        self.action = Action(-1)
+
+
+
+
+    # This method will be called when your bot has to make a move
+    def act(self, gamestate):
+
+        # The current board state will be passed to you in gamestate.pieces, which is a list of 9 integers that follow the following schema:
+        # 0 -> Square is empty
+        # 1 -> Square is O (YOUR BOT'S PIECE)
+        # 2 -> Square is X (OPPONENT'S BOT'S PIECE)
+    
+        # The board squares are indexed row-by-row:
+        # _0_|_1_|_2_
+        # _3_|_4_|_5_
+        #  6 | 7 | 8 
+    
+        # For example, the board state gamestate.pieces=[2, 0, 2, 0, 1, 0, 0, 0, 1] represents the position:
+        # _X_|___|_X_
+        # ___|_O_|___
+        #    |   | O
+    
+        # If you wish to place your piece on index i of the list, then do
+        #     self.action=Action(i)
+        #     return
+
+        # An example bot that places its piece randomly on empty squares
+        while True:
+            i = random.randint(0, 8)
+
+            if (gamestate.pieces[i] == 0):
+                self.action = Action(i)
+                return
+    
+
+
+
+    # Internally used code; do not touch these 2 lines!
+    def run(self):
+        self.act(self.args[0])
